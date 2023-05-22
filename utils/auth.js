5808ceb32ff5bdaa79f2db93630e278441eaf89e
hufflk96 <118147274+hufflk96@users.noreply.github.com>
2023-05-21 21:44:22 -0400

    Delete utils directory

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const withAuth = (req, res, next) => {
-    if (req.session && req.session.logged_in) {
-        next();
-    } else {
-        res.json({message:"Not logged in"});
-    }
-}
-
-module.exports = { withAuth };
