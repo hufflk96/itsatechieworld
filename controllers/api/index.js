b8e5da7063e2ceada9638144d56ce16dd11a9f4a
hufflk96 <118147274+hufflk96@users.noreply.github.com>
2023-05-31 19:43:50 -0400

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
-
-
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
